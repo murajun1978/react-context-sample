3be4f81fd888227c72c2896e725c809cf30c9cbd
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-03-19 21:42:51 +0900

    Add change color button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import MyTheme from './MyTheme';
 import TodoList from './TodoList';
+import TextLabel from './TextLabel';
 
 const todos = ['learning react context', 'blog post'];
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      color: "red"
+    }
+    this.changeToBlue = this.changeToBlue.bind(this)
+  }
+
+  changeToBlue() {
+    this.setState({
+      color: 'blue'
+    })
+  }
+
   render() {
     return (
-      <MyTheme color="red">
+      <MyTheme color={this.state.color}>
+        <button onClick={this.changeToBlue}>
+          <TextLabel>Change Blue</TextLabel>
+        </button>
+
         <TodoList todos={todos} />
       </MyTheme>
     );
